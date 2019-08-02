74db62d4796b86c0e463a90b0551f4184c570ce1
1ba1 <vvvalenti01@yahoo.it>
2019-08-02 15:59:32 +0200

    implemented controller actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
+# BooksController
 class BooksController < ApplicationController
   def index
-    render :json => Book.all
+    render json: Book.all
+  end
+
+  def create
+    Book.create(book_params)
+  end
+
+  def destroy
+    Book.destroy(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :author, :category)
   end
 end
