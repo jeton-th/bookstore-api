85a1c5a7009569c0608a9ea761f4df2b0dd9e31a
Jeton Thaçi <jetonthaci@outlook.com>
2019-08-02 15:00:41 +0200

    add all books api

--- app/controllers/book_controller.rb
+++
@@ -1,9 +0,0 @@
-class BookController < ApplicationController
-  def index
-    @books = Book.all
-
-    respond_to do |format|
-      format.json { render json: @books }
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json }, path: '/' do
+    resources :books
+  end
 end
