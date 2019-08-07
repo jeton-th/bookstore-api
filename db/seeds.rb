3711432a7272cfedc734377afafaf3bfb367d797
Jeton Thaçi <jetonthaci@outlook.com>
2019-08-07 15:57:20 +0200

    Make progress random

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,14 @@ categories = %w[
 100.times do |_n|
   title = Faker::Book.title
   author = Faker::Book.author
+  chapter = rand(1..20)
+  progress = chapter * 5
+
   Book.create!(
     title: title,
     author: author,
-    category: categories[rand(categories.size - 1)]
+    category: categories[rand(categories.size - 1)],
+    chapter: chapter,
+    progress: progress
   )
 end
