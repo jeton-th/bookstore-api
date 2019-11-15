d6a3f486d6479a5a51cb085ce96250cfe9c2645c
1ba1 <vvvalenti01@yahoo.it>
2019-11-15 19:20:46 +0100

    cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins 'herokuapp.com'
 
     resource '*', headers: :any, methods: %I[get post put patch delete options]
   end
