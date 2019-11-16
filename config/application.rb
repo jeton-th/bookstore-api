2d2022070ded7955cdc6fb0cd231141fe3a24a4f
Jeton Thaçi <jetonthaci@outlook.com>
2019-11-16 09:37:25 +0100

    change cors origins

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module BookstoreApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://my-react-bookstore.herokuapp.com/'
         resource(
           '*', 
           headers: :any,
