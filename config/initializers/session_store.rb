245af06d34302cd320c4656167fa1f833441718f
1ba1 <vvvalenti01@yahoo.it>
2019-11-16 10:12:35 +0100

    modify session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://rails-bookstore-api.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', same_site: :strict, domain: 'https://rails-bookstore-api.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', same_site: :strict
 end
