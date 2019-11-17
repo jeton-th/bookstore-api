c8deabe7950da0868ea5464389cd3b1def39d9d3
1ba1 <vvvalenti01@yahoo.it>
2019-11-17 12:05:33 +0100

    same site none

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,12 +3,12 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store,
                                          key: '_session',
-                                         same_site: 'none',
+                                         same_site: :none,
                                          secure: true,
                                          domain: 'https://rails-bookstore-api.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store,
                                          key: '_session',
-                                         same_site: 'none',
+                                         same_site: :none,
                                          secure: true
 end
