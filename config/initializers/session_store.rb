4d98b9090480306f2d758d08c2e4f18bf9e04592
1ba1 <vvvalenti01@yahoo.it>
2019-11-17 11:49:13 +0100

    modify session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,8 +3,8 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store,
                                          key: '_session',
-                                         same_site: :lax,
-                                         secure: 'none',
+                                         same_site: 'none',
+                                         secure: true,
                                          domain: 'https://rails-bookstore-api.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store,
